ffda18c5046c5131a5edaa48b5b9a602f69c19d3
parasharparth <parthparashar6@gmail.com>
2021-11-28 15:01:12 -0800

    Picture of the day commit

--- apod.js
+++ apod.js
@@ -8,9 +8,10 @@ req.send();
 req.addEventListener("load", function(){
 	if(req.status == 200 && req.readyState == 4){
   	var response = JSON.parse(req.responseText);
+    
+    document.getElementById("pic").src = response.hdurl;
     document.getElementById("title").textContent = response.title;
     document.getElementById("date").textContent = response.date;
-    document.getElementById("pic").src = response.hdurl;
     document.getElementById("explanation").textContent = response.explanation;
   }
 })
