86151b2f0634c50afff2b0fa4bba3b10518cce4b
vivianh1234 <56896686+vivianh1234@users.noreply.github.com>
2021-11-30 02:07:30 -0800

    added static liftoff photos

--- photos.js
+++ photos.js
@@ -1,51 +1,56 @@
-const baseUrl = "https://images-api.nasa.gov/search?q=";
-var url = '';
+const liftoffUrls = [
+  "https://images-assets.nasa.gov/image/KSC01pp0436/KSC01pp0436~thumb.jpg",
+  "https://images-assets.nasa.gov/image/KSC-20191205-PH-AWG07_0002/KSC-20191205-PH-AWG07_0002~thumb.jpg",
+  "https://images-assets.nasa.gov/image/KSC-314D-0549_0070/KSC-314D-0549_0070~thumb.jpg",
+  "https://images-assets.nasa.gov/image/KSC-2015-1255/KSC-2015-1255~thumb.jpg",
+  "https://images-assets.nasa.gov/image/s66-42751/s66-42751~thumb.jpg",
+  "https://images-assets.nasa.gov/image/KSC-08pd3696/KSC-08pd3696~thumb.jpg",
+];
 
-function performSearch() {
-  let searchValue = document.querySelector("#photo-search").value;
-  console.log(searchValue);
+const urls = {
+  Liftoff: liftoffUrls,
+};
 
-  url = baseUrl + searchValue;
-  url = encodeURI(url);
-  console.log(url);
+function loadPhotos() {
+  console.log("loading photos");
+  let photoCategory = document.getElementById("photo-select").value;
+  console.log(photoCategory);
+  let urlList = urls[photoCategory];
 
-  let response = fetchPhotos(url);
-//   console.log(response);
+  let photoContainer = document.getElementById("photo-container");
+  let count = 0;
+
+  urlList.forEach((url) => {
+    console.log("appending photo");
+    let newPhoto = document.createElement("div");
+    newPhoto.id = photoCategory + "-image-" + count.toString();
+    newPhoto.className = "container photo";
+    photoContainer.append(newPhoto);
+
+    ReactDOM.render(
+      React.createElement(Photo, {
+        url: url,
+        photoCategory: photoCategory,
+      }),
+      document.getElementById(newPhoto.id)
+    );
+
+    ++count;
+  });
 }
 
-function fetchPhotos(url) {
-    // try {
-    //   let response = await fetch(url);
-    //   response = await response.json();
-    //   console.log(response);
-    // } catch (error) {
-    //   console.log(error);
-    // }
+class Photo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    let alt = this.props.photoCategory + " Image";
 
-//   fetch(url, {
-//     mode: 'no-cors',
-//     headers: {
-//         "Content-Type": "text/plain",
-//         "Access-Control-Allow-Origin": '*',
-//         "Access-Control-Allow-Credentials": "true",
-//     }})
-//     .then((response) => response.json())
-//     .catch((error) => console.log(error));
-
-
-    // let req = new XMLHttpRequest();
-    // req.open("GET", url);
-    // req.send();
-
-    // req.addEventListener("load", function(){
-	//     if(req.status == 200 && req.readyState == 4){
-  	//         let response = JSON.parse(req.responseText);
-    //         console.log("RESPONSE", response.collection);
-    //     }
-    //     else{
-    //         console.log("error");
-    //     }
-    // });
+    return React.createElement(
+      "img",
+      { src: `${this.props.url}`, alt: alt, className: "img-fluid" },
+      null
+    );
+  }
 }
-
